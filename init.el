4c0ccbc61be4f8b48253801dfce8b520dd90e379
kabu1204 <1036058484@qq.com>
2021-12-02 00:23:34 +0800

    update

--- init.el
+++ init.el
@@ -11,13 +11,14 @@
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes '(light-blue))
  '(package-selected-packages
-   '(better-defaults inf-clojure clojure-mode-extra-font-locking cider)))
+   '(ox-pandoc better-defaults inf-clojure clojure-mode-extra-font-locking cider))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "LightBlue" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Fira Code")))))
 
 (package-initialize)
 
@@ -59,3 +60,8 @@
 (setq exec-path (append exec-path '("/usr/local/texlive/2020/bin/x86_64-darwin/")))
 (setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f" "xelatex -interaction nonstopmode %f"))
 
+;;line number
+(global-linum-mode 1)
+(setq linum-format "%d")
+
+;;default font
